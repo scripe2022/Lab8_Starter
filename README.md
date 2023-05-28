b71bd96a0883c3a1df507942893542ac547ef392
jyhptr <im@jyh.sb>
2023-05-27 23:59:27 -0700

    done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 8 - Starter
+
+1. I will put these tests in github actions, that is, each push is automatically executed. Because this can find problems in the early stage of development, specific to each push. At the same time, the test results in github actions are visible to all developers, which is also convenient for other developers to carry out subsequent development and testing. Finally, this saves time and facilitates simultaneous development by multiple people, as the test units can be updated while the code is being developed.
+
+2. No. E2E testing mainly tests whether the user's operation and input meet expectations, and unit testing should be used to test smaller units (functions).
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('phone test 1', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('phone test 2', () => {
+    expect(functions.isPhoneNumber('000-000-0000')).toBe(true);
+});
+
+test('phone test 3', () => {
+    expect(functions.isPhoneNumber('1234567890')).toBe(false);
+});
+
+test('phone test 4', () => {
+    expect(functions.isPhoneNumber('911')).toBe(false);
+});
+
+test('email test 1', () => {
+    expect(functions.isEmail('im@jyh.sb')).toBe(true);
+});
+
+test('email test 2', () => {
+    expect(functions.isEmail('0@gmail.com')).toBe(true);
+});
+
+test('email test 3', () => {
+    expect(functions.isEmail('abc@def')).toBe(false);
+});
+
+test('email test 4', () => {
+    expect(functions.isEmail('@gmail.com')).toBe(false);
+});
+
+test('strong password test 1', () => {
+    expect(functions.isStrongPassword('abc123')).toBe(true);
+});
+
+test('strong password test 2', () => {
+    expect(functions.isStrongPassword('strongPass0')).toBe(true);
+});
+
+test('strong password test 3', () => {
+    expect(functions.isStrongPassword('1234567890')).toBe(false);
+});
+
+test('strong password test 4', () => {
+    expect(functions.isStrongPassword('0123abcdefg!')).toBe(false);
+});
+
+test('date test 1', () => {
+    expect(functions.isDate('77/99/2000')).toBe(true);
+});
+
+test('date test 2', () => {
+    expect(functions.isDate('1/1/2000')).toBe(true);
+});
+
+test('date test 3', () => {
+    expect(functions.isDate('1/1/00')).toBe(false);
+});
+
+test('date test 4', () => {
+    expect(functions.isDate('12252023')).toBe(false);
+});
+
+test('hex color test 1', () => {
+    expect(functions.isHexColor('#000000')).toBe(true);
+});
+
+test('hex color test 2', () => {
+    expect(functions.isHexColor('FfFf01')).toBe(true);
+});
+
+test('hex color test 3', () => {
+    expect(functions.isHexColor('#1234')).toBe(false);
+});
+
+test('hex color test 4', () => {
+    expect(functions.isHexColor('#z12345')).toBe(false);
+});
